117af6cbbfc923bedfefe7cb5beb4cca5145bb3a
Brian Ishii <brianishii@Brians-MacBook-Pro.local>
2017-08-28 22:17:29 -0700

    changed the code so it works like a hash table

--- breadth_first_search/breadth_first_search.py
+++ breadth_first_search/breadth_first_search.py
@@ -1,33 +1,76 @@
 from collections import defaultdict
 from circular_queue.circular_queue import *
 # a Graph is a defaultdict object
+# an AnyValue is an element of any type
+
+class Vertex:
+    
+    def __init__(self, value):
+        self.value = value # an AnyValue
+        self.edges = [] # a list
+        self.visited = False # bool
+
+    def __repr__(self):
+        return "Vertex({!r}, {!r})".format(self.value, self.edges)
+
+    def __eq__(self, other):
+        return (type(other) == Vertex and
+                self.value == other.value and
+                self.edges == other.edges)
 
 class Graph:
 
     def __init__(self):
-        self.graph = defaultdict(list)
+        self.graph = {} # a dictionay
+        self.vertex_count = 0 # an int
+        self.hash_table_size = 100 # an int
 
     def __repr__(self):
-        return "Graph({!r})".format(self.graph)
+        return "Graph({!r}, {!r})".format(self.graph, self.vertex_count)
 
-    def add_edge(self, edge1, edge2):
-        self.graph[edge1].append(edge2)
+    # Graph AnyValue ->
+    # adds a vertex object to the graph
+    def add_vertex(self, value):
+        self.graph[get_key(self.hash_table_size, value)] = Vertex(value)
+        self.vertex_count += 1
+    
+    # Graph AnyValue AnyValue [bool]
+    def add_edge(self, v1, v2, cross_edge=False):
+        key1 = self.is_vertex(v1)
+        key2 = self.is_vertex(v2)
+        if cross_edge is True:
+            self.graph[key2].edges.append(v1)
+        self.graph[key1].edges.append(v2)
 
+
+ 
     def breadth_first_search(self, start):
+        if start not in self.graph:
+            raise KeyError()
         traversal_order = ""
-        visited = [False]*(len(self.graph))
 
         queue = empty_queue()
         
         enqueue(queue, start)
-        visited[start] = True
+        self.graph[start].visited = True
 
         while not is_empty(queue):
-            edge, queue = dequeue(queue)
-            traversal_order += str(edge)
-            for i in self.graph[edge]:
-                if visited[i] == False:
+            vertex, queue = dequeue(queue)
+            traversal_order += str(vertex)
+            for i in self.graph[vertex].edges:
+                if self.graph[i].visited == False:
                     enqueue(queue, i)
-                    visited[i] = True
+                    self.graph[i].visited = True
         return traversal_order
 
+    def is_vertex(self, value):
+        if value not in self.graph:
+            self.add_vertex(value)
+        return get_key(self.hash_table_size, value)
+
+# AnyValue -> int
+# returns the key for the value
+def get_key(hash_table_size, value):
+    if type(value) is not int:
+        value = ord(value[0])
+    return value % hash_table_size
--- breadth_first_search/breadth_first_search_tests.py
+++ breadth_first_search/breadth_first_search_tests.py
@@ -4,6 +4,7 @@ from breadth_first_search import *
 class TestBFS(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        cls.vert = Vertex(1)
         cls.graph_one = Graph()
         cls.graph_one.add_edge(0, 1)
         cls.graph_one.add_edge(0, 2)
@@ -12,8 +13,54 @@ class TestBFS(unittest.TestCase):
         cls.graph_one.add_edge(2, 3)
         cls.graph_one.add_edge(3, 3)
 
-    def test_graph_one(self):
+    def test_vertex_repr(self):
+        self.assertEqual(str(self.vert), "Vertex(1, [])")
+
+    def test_graph_repr_empty(self):
+        temp = Graph()
+        self.assertEqual(str(temp), "Graph({}, 0)")
+
+    def test_add_vertex(self):
+        temp = Graph()
+        temp.add_vertex(0)
+        self.assertEqual(str(temp), "Graph({0: Vertex(0, [])}, 1)")
+        temp.add_vertex("hi")
+        self.assertEqual(str(temp), "Graph({0: Vertex(0, []), " +
+                "4: Vertex('hi', [])}, 2)")
+
+    def test_add_edge_two_vertices(self):
+        pass
+
+    def test_add_edge_one_vertices(self):
+        pass
+
+    def test_add_edge_no_vertices(self):
+        pass
+
+    def test_get_key(self):
+        pass
+
+    
+
+
+"""
+    def test_graph_one_0(self):
+        self.assertEqual(self.graph_one.breadth_first_search(0), "0123") 
+        
+    def test_graph_one_1(self):
+        self.assertEqual(self.graph_one.breadth_first_search(1), "1203") 
+
+    def test_graph_one_2(self):
         self.assertEqual(self.graph_one.breadth_first_search(2), "2031") 
 
+    def test_graph_one_3(self):
+         self.assertEqual(self.graph_one.breadth_first_search(3), "3")
+    
+    def test_add_cross_true(self):
+        temp = Graph()
+        temp.add_edge(0, 1, cross_edge=True)
+        self.assertEqual(temp.breadth_first_search(0), "01")
+        self.assertEqual(temp.breadth_first_search(1), "10")
+"""
 if __name__ == '__main__':
     unittest.main()
