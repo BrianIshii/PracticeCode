24c4ecdb8cf53b1a740a9fc4cc8ade18da89344e
Brian Ishii <brianishii@Brians-MacBook-Pro.local>
2017-08-31 20:18:56 -0700

    changed the functions in hash_table_chaining to be in the class

--- breadth_first_search/hash_table_chaining/hash_table_chaining.py
+++ breadth_first_search/hash_table_chaining/hash_table_chaining.py
@@ -13,6 +13,81 @@ class HashTable:
         return (type(other) == HashTable
                 and self.ls == other.ls
                 and self.cap == other.cap)
+    
+    # HashTable Int Item ->
+    # inserts the Item into the HashTable
+    def insert(self, key, item):
+        h_key = hash(key)
+        index = h_key % self.cap
+        length = len(self.ls[index])
+        x = True
+        if length > 0:
+            self.colls += 1
+            for i in range(length):
+                k, it = self.ls[index][i]
+                if key == k:
+                    self.ls[index][i] = (key, item)
+                    x = False
+        if x is True:
+            self.ls[index].append((key, item))
+            self.length += 1
+        if self.load_factor() > 1.5:
+            self.rehash()
+
+    # HashTable ->
+    # rehashes the table with double the length
+    def rehash(self):
+        length = self.cap * 2 
+        self.colls = 0
+        temp = HashTable()
+        temp.ls = [[] for i in range(length)]
+        temp.cap = length
+        for i in range(self.cap):
+            for j in range(len(self.ls[i])):
+                key, item = self.ls[i][j]
+                temp.insert(key, item)
+        self.ls = temp.ls
+        self.cap = length
+    
+    # HashTable Int -> Item 
+    # returns the Item with the associated key from the HashTable
+    def get(self, key):
+        index = hash(key) % self.cap
+        if self.ls[index] == []:
+            raise LookupError()
+        else:
+            for k, i in self.ls[index]:
+                if key == k:
+                    return i   
+    # HashTable Int -> HashTable
+    # returns the HashTable with the Item of the key removed
+    def remove(self, key):
+        index = hash(key) % self.cap
+        if self.ls[index] == []:
+            raise LookupError()
+        else:
+            for j in range(len(self.ls[index])):
+                k, item = self.ls[index][j]
+                if key == k:
+                    self.ls[index].remove(self.ls[index][j])
+                    self.length -= 1
+                    return
+            raise LookupError()
+    # HashTable -> Int
+    # returns the size of the HashTable
+    def size(self):
+        return self.length
+
+    # HashTable -> Int
+    # returns the current load factor of the hash table
+    def load_factor(self):
+        return (self.length / self.cap)
+
+    # HashTable -> Int
+    # returns the number of collisions that occured during insertions into the HashTable
+    def collisions(self):
+        return self.colls
+
 # -> HashTable
 # returns an empty HashTable with an initial sie of 8
 def empty_hash_table():
--- breadth_first_search/hash_table_chaining/hash_table_chaining_tests.py
+++ breadth_first_search/hash_table_chaining/hash_table_chaining_tests.py
@@ -16,11 +16,11 @@ class HashTableTests(unittest.TestCase):
         cls.two = two
         full = empty_hash_table()
         for i in range(12):
-            full = insert(full, i, "dog")
+            full.insert(i, "dog")
         cls.full = full
         full_rehash = empty_hash_table()
         for i in range(8):
-            full_rehash = insert(full_rehash, i, "dog")
+            full_rehash.insert(i, "dog")
         for i in range(5):
             full_rehash.ls[i].append((i+8, "dog"))
         cls.full_rehash = full_rehash
@@ -28,7 +28,7 @@ class HashTableTests(unittest.TestCase):
         double.ls = [[] for i in range(16)]
         double.cap = 16
         for i in range(13):
-            double = insert(double, i, "dog")
+            double.insert(i, "dog")
         cls.double = double
     
     def test_repr(self):
@@ -40,64 +40,82 @@ class HashTableTests(unittest.TestCase):
 
     def test_insert(self):
         temp = empty_hash_table()
-        self.assertEqual(insert(temp, 1, "one"), self.one)
+        temp.insert(1, "one")
+        self.assertEqual(temp, self.one)
 
     def test_insert_duplicates(self):
+        test = empty_hash_table()
+        test.insert(1, "no")
         temp = empty_hash_table()
-        temp = insert(temp, 1, "no")
-        self.assertEqual(insert(temp, 1, "one"), self.one)
+        temp.insert(1, "no")
+        self.assertEqual(temp, test)
+        temp.insert(1, "one")
+        self.assertEqual(temp, self.one)
 
     def test_insert_rehash(self):
-        self.assertEqual(insert(self.full, 12, "dog"), self.double)
+        full = HashTable()
+        for i in range(12):
+            full.insert(i, "dog")
+        full.insert(12, "dog")
+        self.assertEqual(full, self.double)
 
     def test_rehash(self):
-        self.assertEqual(rehash(self.full_rehash), self.double)
+        full_rehash = HashTable()
+        for i in range(8):
+            full_rehash.insert(i, "dog")
+        for i in range(5):
+            full_rehash.ls[i].append((i+8, "dog"))
+        full_rehash.rehash()
+        self.assertEqual(full_rehash, self.double)
 
     def test_get(self):
-        self.assertEqual(get(self.one, 1), "one")
-        self.assertEqual(get(self.two, 1), "one")
-        self.assertEqual(get(self.two, 9), "two")
+        self.assertEqual(self.one.get(1), "one")
+        self.assertEqual(self.two.get(1), "one")
+        self.assertEqual(self.two.get(9), "two")
 
     def test_get_lookup_error(self):
-        self.assertRaises(LookupError, get, self.empty, 1)
-        self.assertRaises(LookupError, get, self.empty, 20)
+        self.assertRaises(LookupError, self.empty.get, 1)
+        self.assertRaises(LookupError, self.empty.get, 20)
 
     def test_remove(self):
         temp = empty_hash_table()
-        temp = insert(temp, 1, "one")
-        temp = insert(temp, 9, "two")
-        self.assertEqual(remove(temp, 9), self.one)
-        self.assertEqual(remove(temp, 1), self.empty)
+        temp.insert(1, "one")
+        temp.insert(9, "two")
+        temp.remove(9)
+        self.assertEqual(temp, self.one)
+        temp.remove(1)
+        self.assertEqual(temp, self.empty)
     
     def test_remove_lookup_error(self):
-        self.assertRaises(LookupError, remove, self.one, 2)
-        self.assertRaises(LookupError, remove, self.one, 9)
+        self.assertRaises(LookupError, self.one.remove, 2)
+        self.assertRaises(LookupError, self.one.remove, 9)
 
     def test_size(self):
-        self.assertEqual(size(self.empty), 0)
-        self.assertEqual(size(self.one), 1)
-        self.assertEqual(size(self.double), 13)
+        self.assertEqual(self.empty.size(), 0)
+        self.assertEqual(self.one.size(), 1)
+        self.assertEqual(self.double.size(), 13)
 
     def test_load_factor(self):
-        self.assertEqual(load_factor(self.two), 0.25)
+        self.assertEqual(self.two.load_factor(), 0.25)
 
     def test_collisions(self):
-        self.assertEqual(collisions(self.empty), 0)
-        self.assertEqual(collisions(self.double), 0)
-        self.assertEqual(collisions(self.two), 1)
+        self.assertEqual(self.empty.collisions(), 0)
+        self.assertEqual(self.double.collisions(), 0)
+        self.assertEqual(self.two.collisions(), 1)
         full = empty_hash_table()
         for i in range(12):
-            full = insert(full, i, "dog")
-        self.assertEqual(collisions(full), 4)
+            full.insert(i, "dog")
+        temp = full.collisions()
+        self.assertEqual(temp, 4)
 
     def test_all(self):
         temp = empty_hash_table()
-        temp = insert(temp, 1, "one")
-        temp = insert(temp, 2, "two")
+        temp.insert(1, "one")
+        temp.insert(2, "two")
         self.assertEqual(temp.length, 2)
-        temp = remove(temp, 2)
+        temp.remove(2)
         self.assertEqual(temp.length, 1)
-        temp = insert(temp, 9, "nine")
+        temp.insert(9, "nine")
         self.assertEqual(temp.colls, 1)
 
 if __name__ == '__main__':
